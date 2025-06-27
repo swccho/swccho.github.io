a65033811aed49f04fb710278c0a0201ac4a94f9
Ahmed Zobaer <zobayer.me@gmail.com>
2025-06-27 15:07:51 +0600

    code update

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: './', // use relative path for cPanel hosting
   plugins: [react()],
-  base: '/'
 });
