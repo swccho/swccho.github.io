80f6eba606c0e5ec556b7b01aad96f08d96723ee
Ahmed Zobaer <zobayer.me@gmail.com>
2025-06-27 15:10:46 +0600

    codeu pdate

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,5 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: './', // use relative path for cPanel hosting
   plugins: [react()],
 });
